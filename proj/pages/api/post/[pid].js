294c8e084a5f8058fcde74554d263c740fa5bd71
p0ny <p0uy@outlook.com>
2021-05-12 20:34:13 +0800

    feat: post page infinite scroll

--- proj/pages/api/post/[pid].js
+++ proj/pages/api/post/[pid].js
@@ -1,8 +1,23 @@
 import { connectToDatabase } from "../../../utils/mongodb";
 
 export default async (req, res) => {
-  const { pid } = req.query;
+  let { pid, page } = req.query;
+  if (page === undefined) page = 0;
   const { db } = await connectToDatabase();
-  const post = await db.collection("post").findOne({_id: Number(pid)});
-  res.json(post);
+  const reply_cnt = await db.collection("posts").findOne(
+    { _id: Number(pid) },
+    {
+      projection: {
+        reply_cnt: 1,
+      },
+    }
+  );
+  let count = reply_cnt.reply_cnt + 1;
+  const post = await db
+    .collection("post")
+    .find({ _id: Number(pid) })
+    .project({ reply: { $slice: [page * 10, 10] } })
+    .toArray();
+  let data = { ...post[0], hasMore: page * 10 + 10 < count };
+  res.json(data);
 };
--- proj/pages/post/[pid].js
+++ proj/pages/post/[pid].js
@@ -7,71 +7,108 @@ import {
   Text,
   Box,
   Spacer,
+  Button,
+  Divider,
 } from "@chakra-ui/react";
 
 import { format } from "date-fns";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import NavBar from "../../components/NavBar";
 import Comment from "../../components/Comment";
+import fetcher from "../../utils/fetcher";
+import { useSWRInfinite } from "swr";
+import NoMore from "../../components/NoMore";
+import PostListSkeleton from "../../components/PostListSkeleton";
+import { InView } from "react-intersection-observer";
 
-export default function Post() {
-  const router = useRouter();
-  const { pid } = router.query;
-  const [data, setData] = useState({});
+const getKey = (pid) => {
+  return (pageIndex, previousPageData) => {
+    console.log(`pid=${pid}, page=${pageIndex}`);
+    // console.log(previousPageData.hasMore)
+    if (previousPageData && !previousPageData.hasMore) return null;
 
-  useEffect(() => {
-    if (pid === undefined) return;
-    console.log("pid", pid);
-    fetch(`/api/post/${pid}`)
-      .then((res) => res.json())
-      .then((res) => {
-        setData(res);
-      });
-  }, [pid]);
+    return `/api/post/${pid}?page=${pageIndex}`;
+  };
+};
 
+function Page(props) {
+  return props.reply.map((v, index) => (
+    <Flex
+      p={5}
+      shadow="md"
+      borderWidth="1px"
+      w={960}
+      key={index}
+      direction="column"
+      bg="white"
+    >
+      <Flex>
+        <Flex direction="column" align="center" mr={4}>
+          <Avatar name={v.user} />
+          <Text>{v.user}</Text>
+        </Flex>
+        <Text flexGrow={1} maxWidth="860px">
+          {v.content}
+        </Text>
+      </Flex>
+      <Flex justify="flex-end">
+        <Text fontSize="sm" color="gray.400">
+          {format(new Date(v.time), "yyyy-MM-dd HH:mm")}
+        </Text>
+        <Text ml={2}>#{props.pageIndex * 10 + index + 1}</Text>
+      </Flex>
+    </Flex>
+  ));
+}
+
+export default function Post({ pid }) {
+  const { data, size, setSize } = useSWRInfinite(getKey(pid), fetcher);
+  const isEmpty = data?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (data && !data[data.length - 1]?.hasMore);
+  console.log(data);
   return (
     <Box bg="gray.100">
       <NavBar />
       <Center flexDirection="column">
         <Head>
-          <title>{data.title}</title>
+          {/* <title>{data.title}</title> */}
           <meta name="description" content="simple tieba" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <VStack w="980px" spacing={6}>
-          {data.reply &&
-            data.reply.map((v, index) => (
-              <Flex
-                p={5}
-                shadow="md"
-                borderWidth="1px"
-                w={960}
-                key={index}
-                direction="column"
-                bg="white"
-              >
-                <Flex>
-                  <Flex direction="column" align="center" mr={4}>
-                    <Avatar name={v.user} />
-                    <Text>{v.user}</Text>
-                  </Flex>
-                  <Text flexGrow={1} maxWidth="860px">
-                    {v.content}
-                  </Text>
-                </Flex>
-                <Flex justify="flex-end">
-                  <Text fontSize="sm" color="gray.400">
-                    {format(new Date(v.time), "yyyy-MM-dd HH:mm")}
-                  </Text>
-                  <Text ml={2}>#{index + 1}</Text>
-                </Flex>
-              </Flex>
+          {data &&
+            data.map((v, index) => (
+              <Fragment key={index}>
+                <Page reply={v.reply} pageIndex={index} />
+                <Divider my={10} />
+              </Fragment>
             ))}
         </VStack>
         <Comment />
+        {isReachingEnd ? (
+          <NoMore />
+        ) : (
+          <InView
+            as="div"
+            onChange={(inView) => {
+              if (inView) setSize(size + 1);
+            }}
+          >
+            <PostListSkeleton />
+          </InView>
+        )}
       </Center>
     </Box>
   );
+}
+
+export async function getServerSideProps(ctx) {
+  const { pid } = ctx.query;
+  return {
+    props: {
+      pid,
+    },
+  };
 }
